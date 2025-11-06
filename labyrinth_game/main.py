23ef2048e9006cbe01a8352b921b25712594bc80
Valerii Golubtsov <golubcov.valera@yandex.ru>
2025-11-06 09:38:26 +0300

    prep

--- labyrinth_game/main.py
+++ labyrinth_game/main.py
@@ -1,4 +1,15 @@
 #!/usr/bin/env python3
+
+from labyrinth_game.constants import ROOMS
+
+game_state = {
+    'player_inventory': [], # Инвентарь игрока
+    'current_room': 'entrance', # Текущая комната
+    'game_over': False, # Значения окончания игры
+    'steps_taken': 0 # Количество шагов
+}
+  
+
 def main():
     print("Первая попытка запустить проект!")
 
